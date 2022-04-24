6eb75316e7bd21263bb7b2403eb8a7fd846d1eca
Chait <chaitanya1866@gmail.com>
2022-04-24 12:40:48 +0530

    added express

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const path = require("path");
+
+const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("dist"));
+  app.get("*", (req, res) => {
+    req.sendFile(path.resolve(__dirname, "dist", "index.html"));
+  });
+}
+
+app.listen(port, (err) => {
+  if (err) return;
+});
